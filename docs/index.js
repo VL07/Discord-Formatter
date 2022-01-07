6bda740fb80c8e59bcb5e4f20ccc0a20a4d0cdd5
VL07 <79533745+VL07@users.noreply.github.com>
2022-01-07 10:42:38 +0100

    Added basic functionality to the input

---
+++ docs/index.js
@@ -0,0 +1,47 @@
+function setCaretPosition(ctrl, pos) {
+	// Modern browsers
+	if (ctrl.setSelectionRange) {
+		ctrl.focus();
+		ctrl.setSelectionRange(pos, pos);
+		
+	// IE8 and below
+	} else if (ctrl.createTextRange) {
+		var range = ctrl.createTextRange();
+		range.collapse(true);
+		range.moveEnd('character', pos);
+		range.moveStart('character', pos);
+		range.select();
+		}
+}
+
+$('.input > ul > li > button').on('click', function(event) {
+    var cursorPos = $('#inputTextarea').prop('selectionStart');
+    var v = $('#inputTextarea').val();
+    var textBefore = v.substring(0,  cursorPos);
+    var textAfter = v.substring(cursorPos, v.length);
+
+	let before = ""
+	let after = ""
+
+	switch (event.target.id) {
+		case "boldButton":
+			before = "**";
+			after = "**";
+			break;
+		case "italicButton":
+			before = "*";
+			after = "*";
+			break;
+		case "underlineButton":
+			before = "__";
+			after = "__";
+			break;
+		case "strikethroughButton":
+			before = "~~";
+			after = "~~";
+			break;
+	}
+    $('#inputTextarea').val(textBefore + before + after + textAfter);
+	var input = document.getElementById("inputTextarea");
+	setCaretPosition(input, cursorPos + before.length);
+});
